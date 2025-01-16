7c7fd2f6a174ef85f40769206953170b1f773b34
0xGlitchbyte <49317853+0xGlitchbyte@users.noreply.github.com>
2025-01-15 19:54:02 -0500

    wrote encoder logic

--- base64/src/main.zig
+++ base64/src/main.zig
@@ -1,5 +1,12 @@
 const std = @import("std");
 
+pub fn main() !void {
+    const base64 = Base64.init();
+    const bits = 0b10010111;
+    std.debug.print("{d}\n", .{bits & 0b00110000});
+    std.debug.print("Character at index 28: {c}\n", .{base64.char_at(28)});
+}
+
 const Base64 = struct {
     table: *const [64]u8,
 
@@ -35,7 +42,43 @@ pub fn calc_decode_len(input: []const u8) usize {
     return n_output * 3;
 }
 
-pub fn main() !void {
-    const base64 = Base64.init();
-    std.debug.print("Character at index 28: {c}\n", .{base64.char_at(28)});
+pub fn encode(self: Base64, allocator: std.mem.Allocator, input: []const u8) []u8 {
+    if (input.len == 0) {
+        return "";
+    }
+
+    const n_out = calc_encode_len(input);
+    var out = allocator.allc(u8, n_out);
+    var buf = [3]u8{ 0, 0, 0 };
+    var count: u8 = 0;
+    var iout: u64 = 0;
+
+    for (input, 0..) |_, i| {
+        buf[count] = input[i];
+        count += 1;
+        if (count == 3) {
+            out[iout] = self.char_at(buf[0] >> 2);
+            out[iout + 1] = self.char_at((buf[0] & 0x03 << 4) + (buf[1] >> 4));
+            out[iout + 2] = self.char_at((buf[1] & 0x0f) << 2) + (buf[2] & 0x3f);
+            iout += 4;
+            count = 0;
+        }
+    }
+
+    if (count == 1) {
+        out[iout] = self.char_at(buf[0] >> 2);
+        out[iout + 1] = self.char_at((buf[0] & 0x03) << 4);
+        out[iout + 2] = '=';
+        out[iout + 3] = '=';
+    }
+
+    if (count == 2) {
+        out[iout] = self.char_at(buf[0] >> 2);
+        out[iout + 1] = self.char_at(((buf[0] & 0x03) << 4) + (buf[1] >> 4));
+        out[iout + 2] = self.char_at((buf[1] & 0x0f) << 2);
+        out[iout + 3] = '=';
+        iout += 4;
+    }
+
+    return out;
 }
